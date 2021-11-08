57cd0892f52dd58e86a3ef72a9dc3d8c4beedb11
Yurii Nuzhniy <uramen@ukr.net>
2021-11-08 20:50:22 +0200

    rename text1>line

--- src/Stepic/PrintTextPerRole2.java
+++ src/Stepic/PrintTextPerRole2.java
@@ -1,7 +1,37 @@
 package Stepic;
 //this task from the stepic site
 //Write a method that will group the lines by role, number them and return the result in the form of ready-made text
-public class PrintTextPerRole2 {
+class ForSearching{
+    String x1[];
+    String x2[];
+    StringBuilder toReturn;
+
+    ForSearching(String y1[],String y2[]){
+        x1=y1;
+        x2=y2;
+    }
+    StringBuilder toreturn() {
+        toReturn = new StringBuilder();
+        String[] forSearching=new String[x1.length];
+        for (int i = 0; i < x1.length; i++) {
+            forSearching[i] = x1[i] + ":";
+            toReturn.append(forSearching[i]);
+            toReturn.append('\n');
+            for (int j = 0; j < x2.length; j++) {
+                if (x2[j].startsWith(forSearching[i] + " ")) {
+                    String toReplace = (j + 1) + ")";
+                    toReturn.append(toReplace);
+                    toReturn.append(x2[j].substring(forSearching[i].length()));
+                    toReturn.append('\n');
+                }
+            }
+            toReturn.append('\n');
+        }
+        return toReturn;
+    }
+}
+
+class PrintTextPerRole2 {
     public static void main(String[] args) {
 
     String roles[] ={"Городничий","Аммос Федорович","Артемий Филиппович","Лука Лукич"};
@@ -13,24 +43,7 @@ public class PrintTextPerRole2 {
             "Аммос Федорович: Вот те на!",
             "Артемий Филиппович: Вот не было заботы, так подай!",
             "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
-        //array for variables to search by role
-    String[] forSearching=new String[roles.length];
-   //create a variable to return
-        StringBuilder toReturn =new StringBuilder();
-        for(int i =0;i< roles.length;i++){
-            forSearching[i] = roles[i]+":";
-            toReturn.append(forSearching[i]);
-            toReturn.append('\n');
-            for(int j=0;j< textLines.length;j++){
-                if(textLines[j].startsWith(forSearching[i]+ " ")) {
-                    String toReplace = (j+1)+")";
-                    toReturn.append(toReplace);
-                    toReturn.append(textLines[j].substring(forSearching[i].length()));
-                    toReturn.append('\n');
-            }
-        }
-            toReturn.append('\n');
-   }
-        System.out.println(toReturn);
-}
+       ForSearching text = new ForSearching(roles, textLines);
+       System.out.println(text.toreturn());
+    }
 }
