e7a0a053ab9022b50fa6283cb94e4ffbf1eaa10a
Yurii Nuzhniy <uramen@ukr.net>
2021-11-17 20:10:02 +0200

    Topic 10 was studied: exception handling

---
+++ src/java_10thEdition/exceptionHandling/ExceptionDemo.java
@@ -0,0 +1,31 @@
+package java_10thEdition.exceptionHandling;
+
+class MyException extends Exception{
+    private int detail;
+
+    MyException(int a){
+        detail = a;
+    }
+    public String toString(){
+return "MyException[ " + detail+" ] ";
+    }
+}
+
+public class ExceptionDemo {
+    static void compute(int a) throws MyException{
+        System.out.println("compute metod called( " + a + ") ");
+        if (a > 10){
+            throw new MyException(a);
+        }
+        System.out.println("Normal finish");
+    }
+
+    public static void main(String[] args) {
+        try {
+            compute(1);
+            compute(20);
+        }catch (MyException e){
+            System.out.println("caught an exception: " + e);
+        }
+    }
+}
